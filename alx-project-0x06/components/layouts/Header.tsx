ca9f963863b5d59c93fb602e32c062b46c9f52fc
XpertDude <braodby@gmail.com>
2025-08-02 09:19:47 +0100

    Include a Redux for global state management

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
 
   const pathname = usePathname()
-  const { count } = useCount()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
-    <header className="fixed w-full bg-white shadow-md p-3 m-5">
+    <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
         <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
